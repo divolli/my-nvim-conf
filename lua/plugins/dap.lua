47874fc7b10bd15efa4665056f2e9df7ce9f5b82
divolli <dpavlysh@gmail.com>
2025-09-10 19:59:54 +0200

    Added more precise debug conf

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,12 +11,18 @@ return {
 			local dap = require("dap")
 			local ui = require("dapui")
 
+			-- Setup dapui
+			ui.setup()
+
+			-- Setup virtual text
+			require("nvim-dap-virtual-text").setup()
+
+			-- C++ configuration
 			dap.adapters.cpp = {
 				type = "executable",
 				command = "gdb",
 				args = { "-i", "dap" },
 			}
-
 			dap.configurations.cpp = {
 				{
 					name = "Launch C++ program",
@@ -30,52 +36,81 @@ return {
 					args = {},
 				},
 			}
-
 			dap.configurations.c = dap.configurations.cpp
 
+			-- Python configuration (improved)
 			dap.adapters.python = {
 				type = "executable",
 				command = "python3",
-				args = { "-Xfrozen_modules=off", "-m", "debugpy.adapter" },
+				args = { "-m", "debugpy.adapter" },
 			}
-
 			dap.configurations.python = {
 				{
 					type = "python",
 					request = "launch",
 					name = "Launch Python file",
-					program = "${file}", -- current buffer
+					program = "${file}",
+					console = "integratedTerminal",
 					pythonPath = function()
+						-- Try to find python in virtual env first
+						local venv = os.getenv("VIRTUAL_ENV")
+						if venv then
+							return venv .. "/bin/python"
+						end
+						return "python3"
+					end,
+				},
+				{
+					type = "python",
+					request = "launch",
+					name = "Launch Python with args",
+					program = "${file}",
+					console = "integratedTerminal",
+					args = function()
+						local args_str = vim.fn.input("Arguments: ")
+						if args_str == "" then
+							return {}
+						end
+						return vim.split(args_str, " ", { trimempty = true })
+					end,
+					pythonPath = function()
+						local venv = os.getenv("VIRTUAL_ENV")
+						if venv then
+							return venv .. "/bin/python"
+						end
 						return "python3"
 					end,
 				},
 			}
 
-			vim.keymap.set("n", "<space>sb", dap.toggle_breakpoint) -- Set break point
-			vim.keymap.set("n", "<space>gb", dap.run_to_cursor) -- Run cursor
-
-			-- Eval var under cursor
+			-- Keymaps
+			vim.keymap.set("n", "<space>sb", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor, { desc = "Run to cursor" })
 			vim.keymap.set("n", "<space>?", function()
 				require("dapui").eval(nil, { enter = true })
-			end)
+			end, { desc = "Evaluate expression" })
 
-			vim.keymap.set("n", "<F1>", dap.continue)
-			vim.keymap.set("n", "<F2>", dap.step_into)
-			vim.keymap.set("n", "<F3>", dap.step_over)
-			vim.keymap.set("n", "<F4>", dap.step_out)
-			vim.keymap.set("n", "<F5>", dap.step_back)
-			vim.keymap.set("n", "<F13>", dap.restart)
+			-- Function keys for debugging
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue" })
+			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over" })
+			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into" })
+			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out" })
+			vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Restart debugger" })
+			vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate debugger" })
 
-			dap.listeners.before.attach.dapui_config = function()
+			-- Conditional breakpoint
+			vim.keymap.set("n", "<leader>db", function()
+				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end, { desc = "Set conditional breakpoint" })
+
+			-- DAP UI listeners
+			dap.listeners.after.event_initialized["dapui_config"] = function()
 				ui.open()
 			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
+			dap.listeners.before.event_terminated["dapui_config"] = function()
 				ui.close()
 			end
-			dap.listeners.before.event_exited.dapui_config = function()
+			dap.listeners.before.event_exited["dapui_config"] = function()
 				ui.close()
 			end
 		end,
